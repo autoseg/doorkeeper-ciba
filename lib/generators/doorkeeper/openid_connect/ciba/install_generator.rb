7aaadaf610787fd40f6b8ca4e7cfa261ce18d965
Barbara Carrijo <barbarahcarrijo@gmail.com>
2022-01-27 15:27:40 -0300

    style: formatting code

--- lib/generators/doorkeeper/openid_connect/ciba/install_generator.rb
+++ lib/generators/doorkeeper/openid_connect/ciba/install_generator.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 module Doorkeeper
-	module OpenidConnect
-	  module Ciba
-	    class InstallGenerator < ::Rails::Generators::Base
-	      include ::Rails::Generators::Migration
-	      source_root File.expand_path('templates', __dir__)
-	      desc 'Installs Doorkeeper Open Id Connect CIBA Support.'
-	
-	      def install
-	        template 'initializer.rb', 'config/initializers/doorkeeper_openid_connect_ciba.rb'
-			puts("ALERT: Please edit config/initializers/doorkeeper_openid_connect_ciba.rb and configure mandatory entries")
-	        #copy_file File.expand_path('../../../../../config/locales/en.yml', __dir__), 'config/locales/doorkeeper_openid_connect_ciba.en.yml'
-	        route 'use_doorkeeper_openid_connect_ciba'
-	      end
-	    end
-	  end
-	end
+  module OpenidConnect
+    module Ciba
+      class InstallGenerator < ::Rails::Generators::Base
+        include ::Rails::Generators::Migration
+        source_root File.expand_path('templates', __dir__)
+        desc 'Installs Doorkeeper Open Id Connect CIBA Support.'
+
+        def install
+          template 'initializer.rb', 'config/initializers/doorkeeper_openid_connect_ciba.rb'
+          puts("ALERT: Please edit config/initializers/doorkeeper_openid_connect_ciba.rb and configure mandatory entries")
+          #copy_file File.expand_path('../../../../../config/locales/en.yml', __dir__), 'config/locales/doorkeeper_openid_connect_ciba.en.yml'
+          route 'use_doorkeeper_openid_connect_ciba'
+        end
+      end
+    end
+  end
 end
--- lib/generators/doorkeeper/openid_connect/ciba/migration_generator.rb
+++ lib/generators/doorkeeper/openid_connect/ciba/migration_generator.rb
@@ -3,51 +3,49 @@
 require 'rails/generators/active_record'
 
 module Doorkeeper
-	module OpenidConnect
-	  module Ciba
-	    class MigrationGenerator < ::Rails::Generators::Base
-	      include ::Rails::Generators::Migration
-	      source_root File.expand_path('templates', __dir__)
-	      desc 'Installs Doorkeeper OpenID Connect Ciba migration file.'
-	
-	      def install
-	        migration_template(
-	          'create_ciba_backchannel_tables.rb.erb',
-	          'db/migrate/create_ciba_backchannel_tables.rb',
-	          migration_version: migration_version
-	        )
-	        migration_template(
-	          'add_ciba_reqauthid_to_oauth_token.rb.erb',
-	          'db/migrate/add_ciba_reqauthid_to_oauth_token.rb',
-	          migration_version: migration_version
-	        )
-	        migration_template(
-	          'add_ciba_notify_columns_to_oauth_applications.rb.erb',
-	          'db/migrate/add_ciba_notify_columns_to_oauth_applications.rb',
-	          migration_version: migration_version
-	        )
-	        migration_template(
-	          'add_ciba_backchannel_auth_requests_last_token_get.rb.erb',
-	          'db/migrate/add_ciba_backchannel_auth_requests_last_token_get.rb',
-	          migration_version: migration_version
-	        )
+  module OpenidConnect
+    module Ciba
+      class MigrationGenerator < ::Rails::Generators::Base
+        include ::Rails::Generators::Migration
+        source_root File.expand_path('templates', __dir__)
+        desc 'Installs Doorkeeper OpenID Connect Ciba migration file.'
 
+        def install
+          migration_template(
+            'create_ciba_backchannel_tables.rb.erb',
+            'db/migrate/create_ciba_backchannel_tables.rb',
+            migration_version: migration_version
+          )
+          migration_template(
+            'add_ciba_reqauthid_to_oauth_token.rb.erb',
+            'db/migrate/add_ciba_reqauthid_to_oauth_token.rb',
+            migration_version: migration_version
+          )
+          migration_template(
+            'add_ciba_notify_columns_to_oauth_applications.rb.erb',
+            'db/migrate/add_ciba_notify_columns_to_oauth_applications.rb',
+            migration_version: migration_version
+          )
+          migration_template(
+            'add_ciba_backchannel_auth_requests_last_token_get.rb.erb',
+            'db/migrate/add_ciba_backchannel_auth_requests_last_token_get.rb',
+            migration_version: migration_version
+          )
 
+        end
 
-	      end
-	
-	      def self.next_migration_number(dirname)
-	        ActiveRecord::Generators::Base.next_migration_number(dirname)
-	      end
-	
-	      private
-	
-	      def migration_version
-	        if ActiveRecord::VERSION::MAJOR >= 5
-	          "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
-	        end
-	      end
-	    end
-	  end
-	end
+        def self.next_migration_number(dirname)
+          ActiveRecord::Generators::Base.next_migration_number(dirname)
+        end
+
+        private
+
+        def migration_version
+          if ActiveRecord::VERSION::MAJOR >= 5
+            "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
+          end
+        end
+      end
+    end
+  end
 end
